5b37b21df55b04f85ba4f36e0c5b465aae76b808
kifferkniffer <kifferkniffer@gmx.de>
2013-06-29 19:20:03 +0200

    Update Main.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,13 +6,15 @@ int main ()
 	// initialize the engine
 	GameEngine game( "Engine Test v2.0" );
 	game.run( game.build<IntroState>( true ) );
+	sf::Clock clock;
 
 	// main loop
 	while( game.running() )
 	{
+		sf::Time elapsed = clock.restart();
 		game.nextState();
-		game.update();
-		game.draw();
+		game.update(elapsed);
+		game.draw(elapsed);
 	}
 
 	// leaving the scope of 'game' will cleanup the engine
